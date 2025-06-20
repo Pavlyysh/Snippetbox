2bc8e095998bc06d1c570dc974106009d218fb92
Pavlyysh <pgfu619@mail.ru>
2025-06-20 18:53:30 +0300

    index.html

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
@@ -15,14 +16,46 @@ func main() {
 	// HTTP-запросы к статическим файлам из папки "./ui/static".
 	// Обратите внимание, что переданный в функцию http.Dir путь
 	// является относительным корневой папке проекта
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+	fileServer := http.FileServer(neutredFileSystem{http.Dir("./static")})
 
 	// Используем функцию mux.Handle() для регистрации обработчика для
 	// всех запросов, которые начинаются с "/static/". Мы убираем
 	// префикс "/static" перед тем как запрос достигнет http.FileServer
+	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("start web-server on port :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+type neutredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neutredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
