f995bd80b2f9446e425e84842f751199ce255f17
Pavlyysh <pgfu619@mail.ru>
2025-06-23 16:06:21 +0300

    new file routes.go

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -44,24 +44,6 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	// Используем методы из структуры в качестве обработчиков маршрутов.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	// Инициализируем FileServer, он будет обрабатывать
-	// HTTP-запросы к статическим файлам из папки "./ui/static".
-	// Обратите внимание, что переданный в функцию http.Dir путь
-	// является относительным корневой папке проекта
-	fileServer := http.FileServer(neutredFileSystem{http.Dir("./ui/static")})
-
-	// Используем функцию mux.Handle() для регистрации обработчика для
-	// всех запросов, которые начинаются с "/static/". Мы убираем
-	// префикс "/static" перед тем как запрос достигнет http.FileServer
-	mux.Handle("/static", http.NotFoundHandler())
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	// Инициализируем новую структуру http.Server. Мы устанавливаем поля Addr и Handler, так
 	// что сервер использует тот же сетевой адрес и маршруты, что и раньше, и назначаем
 	// поле ErrorLog, чтобы сервер использовал наш логгер
@@ -69,7 +51,7 @@ func main() {
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(), // Вызов нового метода app.routes()
 	}
 
 	// Значение, возвращаемое функцией flag.String(), является указателем на значение
