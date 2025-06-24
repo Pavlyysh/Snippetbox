3b0e01b59810713fa8437add05c16fd885768e19
Pavlyysh <pgfu619@mail.ru>
2025-06-24 18:02:06 +0300

    method Latest and Get

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
+	"pavlyysh/snippetbox/pkg/models"
 	"strconv"
 )
 
@@ -15,38 +16,48 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Инициализируем срез содержащий пути к двум файлам. Обратите внимание, что
-	// файл home.page.tmpl должен быть *первым* файлом в срезе.
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-
-	// Используем функцию template.ParseFiles() для чтения файла шаблона.
-	// Если возникла ошибка, мы запишем детальное сообщение ошибки и
-	// используя функцию http.Error() мы отправим пользователю
-	// ответ: 500 Internal Server Error (Внутренняя ошибка на сервере)
-	ts, err := template.ParseFiles(files...)
+	s, err := app.snippets.Latest()
 	if err != nil {
-		// Поскольку обработчик home теперь является методом структуры application
-		// он может получить доступ к логгерам из структуры.
-		// Используем их вместо стандартного логгера от Go.
-		app.serverError(w, err) // Использование помощника serverError()
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, err)
 		return
 	}
 
-	// Затем мы используем метод Execute() для записи содержимого
-	// шаблона в тело HTTP ответа. Последний параметр в Execute() предоставляет
-	// возможность отправки динамических данных в шаблон.
-	err = ts.Execute(w, nil)
-	if err != nil {
-		// Обновляем код для использования логгера-ошибок
-		// из структуры application.
-		app.serverError(w, err) // Использование помощника serverError()
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	for _, snippet := range s {
+		fmt.Fprintf(w, "%v\n", snippet)
 	}
+
+	// // Инициализируем срез содержащий пути к двум файлам. Обратите внимание, что
+	// // файл home.page.tmpl должен быть *первым* файлом в срезе.
+	// files := []string{
+	// 	"./ui/html/home.page.tmpl",
+	// 	"./ui/html/base.layout.tmpl",
+	// 	"./ui/html/footer.partial.tmpl",
+	// }
+
+	// // Используем функцию template.ParseFiles() для чтения файла шаблона.
+	// // Если возникла ошибка, мы запишем детальное сообщение ошибки и
+	// // используя функцию http.Error() мы отправим пользователю
+	// // ответ: 500 Internal Server Error (Внутренняя ошибка на сервере)
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	// Поскольку обработчик home теперь является методом структуры application
+	// 	// он может получить доступ к логгерам из структуры.
+	// 	// Используем их вместо стандартного логгера от Go.
+	// 	app.serverError(w, err) // Использование помощника serverError()
+	// 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// // Затем мы используем метод Execute() для записи содержимого
+	// // шаблона в тело HTTP ответа. Последний параметр в Execute() предоставляет
+	// // возможность отправки динамических данных в шаблон.
+	// err = ts.Execute(w, nil)
+	// if err != nil {
+	// 	// Обновляем код для использования логгера-ошибок
+	// 	// из структуры application.
+	// 	app.serverError(w, err) // Использование помощника serverError()
+	// 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	// }
 
 }
 
@@ -59,7 +70,20 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "snippet with ID %d\n", id)
+	// Вызываем метода Get из модели Snipping для извлечения данных для
+	// конкретной записи на основе её ID. Если подходящей записи не найдено,
+	// то возвращается ответ 404 Not Found (Страница не найдена).
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%v", s)
 }
 
 // Меняем сигнатуру обработчика createSnippet, чтобы он определялся как метод
